c3683980a074ad12b138334cb2916ae51bb433c2
dancha10 <mdan201510@gmail.com>
2022-05-15 02:54:50 +0700

    fix: review modal logic

--- src/widgets/review-list/model/review-modal.ts
+++ src/widgets/review-list/model/review-modal.ts
@@ -1,45 +1,75 @@
 import { createEffect, createEvent, createStore, sample } from 'effector'
+import { createForm } from 'effector-forms'
+import * as yup from 'yup'
 
 import { editReview } from 'shared/api'
 import { setNotify } from 'entities/notification/model'
-import { getReviewsList } from 'widgets/review-list/model/reviewsSort'
 import { ActionBarModel } from 'features/action-bar'
-import { Types } from 'shared/lib'
+import { createRule, Types } from 'shared/lib'
+import { REQUIRED_MESSAGE } from 'shared/const'
+
+import { getReviewsList } from './reviewsSort'
 
 export type changeReviewType = {
 	id: string
 	text: string
 }
 
+export const reviewEditForm = createForm({
+	fields: {
+		review: {
+			init: '',
+			rules: [
+				createRule({
+					name: 'review',
+					schema: yup.string().required(REQUIRED_MESSAGE),
+				}),
+			],
+		},
+	},
+	validateOn: ['change', 'submit'],
+})
+
 export const toggledOpenModal = createEvent()
-export const $isOpen = createStore(false).on(toggledOpenModal, (prev, _) => !prev)
+export const $isOpen = createStore(false).on(toggledOpenModal, prev => !prev)
 
 export const getCurrentReview = createEvent<changeReviewType>()
-export const $currentReview = createStore<changeReviewType>({
-	id: '',
-	text: '',
-}).on(getCurrentReview, (_, review) => review)
+
+sample({
+	clock: getCurrentReview,
+	fn: review => ({ review: review.text }),
+	target: reviewEditForm.setForm,
+})
 
 sample({
 	clock: getCurrentReview,
 	target: toggledOpenModal,
 })
 
-export const editedReview = createEvent<string>()
 export const changeReviewFx = createEffect<changeReviewType, Types.ListReviewsType, Error>(
 	async ({ id, text }) => await editReview(id, text)
 )
 
 sample({
-	clock: editedReview,
+	clock: sample({
+		clock: reviewEditForm.submit,
+		source: reviewEditForm.$values,
+		fn: text => text.review,
+	}),
 	source: getCurrentReview,
-	fn: (source, clock) => ({ id: source.id, text: clock }),
+	fn: (source, text) => ({ id: source.id, text }),
 	target: changeReviewFx,
 })
 
 sample({
 	clock: changeReviewFx.doneData,
 	fn: () => ({ type: 'success', title: 'Успешно', message: 'Отзыв успешно отредактирован' }),
+	target: setNotify,
+})
+
+sample({
+	clock: changeReviewFx.failData,
+	fn: () => ({ type: 'error', title: 'Ошибка', message: 'Произошла ошибка :(' }),
 	target: setNotify,
 })
 
--- src/widgets/review-list/ui/modal/index.tsx
+++ src/widgets/review-list/ui/modal/index.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react'
+import { FC, FormEvent } from 'react'
 import { useStore } from 'effector-react'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { useForm } from 'effector-forms'
 
 import { Modal } from 'shared/ui/atoms/modal'
 import { ToggleModal } from 'features/toggle-modal'
@@ -8,34 +8,19 @@ import { Textarea } from 'shared/ui/atoms/textarea'
 import { Button } from 'shared/ui/atoms/button'
 import { CubeLoader } from 'shared/ui/atoms/cube-loader'
 
-import { $currentReview, $isOpen, changeReviewFx, editedReview, toggledOpenModal } from '../../model/review-modal'
+import { $isOpen, changeReviewFx, reviewEditForm, toggledOpenModal } from '../../model/review-modal'
 
 import './style.scss'
 
-interface IReviewEditModal {
-	review: string
-}
-
 export const ReviewEditModal: FC = () => {
-	const currentReview = useStore($currentReview)
 	const isOpen = useStore($isOpen)
 
-	const toggle = () => {
-		toggledOpenModal()
-		reset()
+	const { fields, submit, hasError } = useForm(reviewEditForm)
+
+	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		submit()
 	}
-
-	const {
-		register,
-		handleSubmit,
-		reset,
-		formState: { errors },
-	} = useForm<IReviewEditModal>({ mode: 'onChange' })
-
-	const onSubmit: SubmitHandler<IReviewEditModal> = data => {
-		editedReview(data.review)
-	}
-
 	const isLoading = useStore(changeReviewFx.pending)
 
 	return (
@@ -44,20 +29,22 @@ export const ReviewEditModal: FC = () => {
 			<div className='review-edit'>
 				<div className='review-edit__header'>
 					<h4>Отзыв</h4>
-					<ToggleModal toggle={toggle} />
+					<ToggleModal toggle={toggledOpenModal} />
 				</div>
-				<form className='review-edit__container' onSubmit={handleSubmit(onSubmit)}>
+				<form className='review-edit__container' onSubmit={onSubmit}>
 					<Textarea
 						maxLength={200}
 						label='Отзыв'
 						placeholder='Edit'
-						isError={false}
-						value={currentReview.text}
-						validation={{ ...register('review') }}
+						isError={hasError('review')}
+						onChange={fields.review.onChange}
+						value={fields.review.value}
 					/>
 					<div className='review-edit__area-button'>
-						<Button.Dark type='submit'>Подтвердить редактирование</Button.Dark>
-						<Button.Dark color='red' type='button' onClickHandler={toggle}>
+						<Button.Dark type='submit' disabled={hasError('review')}>
+							Подтвердить редактирование
+						</Button.Dark>
+						<Button.Dark color='red' type='button' onClickHandler={toggledOpenModal}>
 							Отмена
 						</Button.Dark>
 					</div>
