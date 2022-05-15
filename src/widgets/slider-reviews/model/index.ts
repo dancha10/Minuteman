058d2b47c0dbbbc610ef9c3fafe3c0253b70de56
dancha10 <mdan201510@gmail.com>
2022-05-15 13:53:40 +0700

    fix: sort review in slider

--- src/widgets/slider-reviews/model/index.ts
+++ src/widgets/slider-reviews/model/index.ts
@@ -7,12 +7,15 @@ export const getSliderReviews = createEvent()
 
 export const sliderReviewsFx = createEffect<void, Types.ListReviewsType[], Error>(async () => await getReviews())
 
-export const $userReviews = createStore<Types.ListReviewsType[]>([]).on(
-	sliderReviewsFx.doneData,
-	(_, reviews) => reviews
-)
+export const $userReviews = createStore<Types.ListReviewsType[]>([])
 
 sample({
 	clock: getSliderReviews,
 	target: sliderReviewsFx,
+})
+
+sample({
+	clock: sliderReviewsFx.doneData,
+	fn: reviews => reviews.filter(review => review.status === 'approved'),
+	target: $userReviews,
 })
